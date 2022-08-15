60c150a864cb40cee70957087faa6b259445a75b
Robert10101010 <109917542+Robert10101010@users.noreply.github.com>
2022-08-15 20:03:30 +0200

    Add files via upload

--- HF-4.php
+++ HF-4.php
@@ -1,7 +1,7 @@
 <?php
 $tomb=array();
 $x=1;
-
+$sum=0;
 for($i=0; $i<4; $i++) {
     for($j=0; $j<4; $j++) {
         $tomb[$i][$j]=$x;
@@ -10,10 +10,13 @@ for($i=0; $i<4; $i++) {
 }
 for($i=0; $i<4; $i++) {
     for($j=0; $j<4; $j++) {     
-    echo $random = rand(1, 100). ' ';
+    echo $tomb[$i][$j]= rand(1, 100). ' ';
+   if($j==0){
+    $sum += $tomb[$i][$j];
+   }
 } 
     echo"<br>";
 }
-
-
+    echo"<br>";
+    echo "Eredmeny:<br>". $sum;
 ?>
