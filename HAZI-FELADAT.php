cf07c2898fe5c15c7dec3d3ef87155cb78365f20
Robert10101010 <109917542+Robert10101010@users.noreply.github.com>
2022-08-15 18:36:40 +0200

    Add files via upload

--- HAZI-FELADAT.php
+++ HAZI-FELADAT.php
@@ -1,7 +1,9 @@
 <?php
 $tomb=array();
 $x=1;
-$sum=0;
+$paros=0;
+$paratlan=0;
+
 for($i=0; $i<3; $i++) {
     for($j=0; $j<3; $j++) {
         $tomb[$i][$j]=$x;
@@ -10,9 +12,17 @@ for($i=0; $i<3; $i++) {
 }
 for($i=0; $i<3; $i++) {
     for($j=0; $j<3; $j++) {
-    echo $tomb[$i][$j] . ' ';
+        echo $tomb[$i][$j] . ' ';
+        if ($tomb[$i][$j]%2 == 0) {
+            $paros++; 
+        } else {
+            $paratlan++;  
+         } 
     }
-    echo"<br>";
-}
+        echo "<br>";
+} 
+        echo "<br>";
 
+        echo "Paros: $paros" . "<br />";
+        echo "Paratlan: $paratlan";
 ?>
