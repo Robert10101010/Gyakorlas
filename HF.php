da614f46c3aad32cd8df5b38ba20a35580b35a9c
ratesz94 <111195282+ratesz94@users.noreply.github.com>
2022-08-15 13:29:33 +0200

    Update HF.php

--- HF.php
+++ HF.php
@@ -6,7 +6,11 @@ for($i=0; $i<10; $i++) {
 }
 for($i=0; $i<10; $i++) {     
     echo $tomb[$i]. ' ' ;
-        if ($tomb[$i] % 2 == 0) {
+        if ($tomb[$i] % 2 == 0) { 
+            // ha az ellenkezőjét akarod ellenőrizni, vagyis, hogy páratlan a szám akkor negálni kell a feltételt
+            // tehát ez: if($tomb[$i] % 2 == 0) azt nézi hogy páros e a szám
+            // ez pedig azt, hogy páratlan-e if($tomb[$i] % 2 != 0)
+            // Ha igy csinálod nem fog kelleni az üres if. Nem hiba az sem ahogy csináltad, de ez a szebb megoldás.
      
         } else {
         $sum += $tomb[$i];
