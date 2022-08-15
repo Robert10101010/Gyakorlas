3591de3dd5fe14de6c8e8a48975920975684431c
Robert10101010 <109917542+Robert10101010@users.noreply.github.com>
2022-08-15 20:18:06 +0200

    Add files via upload

--- HF-4.php
+++ HF-4.php
@@ -2,6 +2,9 @@
 $tomb=array();
 $x=1;
 $sum=0;
+$sum1=0;
+$sum2=0;
+$sum3=0;
 for($i=0; $i<4; $i++) {
     for($j=0; $j<4; $j++) {
         $tomb[$i][$j]=$x;
@@ -14,9 +17,16 @@ for($i=0; $i<4; $i++) {
    if($j==0){
     $sum += $tomb[$i][$j];
    }
+   elseif($j==1){
+    $sum1 += $tomb[$i][$j];
+   }elseif($j==2){
+    $sum2 += $tomb[$i][$j];
+   }elseif($j==3){
+    $sum3 += $tomb[$i][$j];
+   }
 } 
     echo"<br>";
 }
     echo"<br>";
-    echo "Eredmeny:<br>". $sum;
+    echo "Eredmeny:<br>". $sum.' ', $sum1.' ', $sum2.' ', $sum3;
 ?>
