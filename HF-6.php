f945bec7a51c3344c33f778953703d7d11a76dfe
Robert10101010 <109917542+Robert10101010@users.noreply.github.com>
2022-08-16 20:08:45 +0200

    Add files via upload

---
+++ HF-6.php
@@ -0,0 +1,37 @@
+<?php
+$tomb1=array();
+$x=rand(1,50);
+for($i=0; $i<3; $i++) {
+    for($j=0; $j<3; $j++) {
+        $tomb1[$i][$j]=$x;
+        $x++;
+    }
+}
+$tomb2=array();
+$x=rand(1,50);
+for($i=0; $i<3; $i++) {
+    for($j=0; $j<3; $j++) {
+        $tomb2[$i][$j]=$x;
+        $x++;
+    }
+}
+echo"Tomb1: <br>";
+for($i=0; $i<3; $i++) {
+    
+    for($j=0; $j<3; $j++) {    
+    echo $tomb1[$i][$j] . ' ';
+    
+}
+    echo"<br>";
+}
+echo"<br>";
+echo"Tomb2: ";
+for($i=0; $i<3; $i++) {
+    echo"<br>";
+    for($j=0; $j<3; $j++) {    
+    echo $tomb1[$j][$i] . ' ';  
+        
+        }   
+}
+
+?>
