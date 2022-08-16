7137cbc68fe9b6e280087b12f23ed911711e9f49
Robert10101010 <109917542+Robert10101010@users.noreply.github.com>
2022-08-16 18:59:35 +0200

    Add files via upload

---
+++ HF-5.php
@@ -0,0 +1,19 @@
+<?php
+	$tomb = array();			
+	for($i=0;$i<7;) {
+		$x = rand (1, 40);
+		if(array( $x, $tomb) )  {
+			$tomb[$i] = $x;
+			$i++;
+        }
+	}
+    echo "eredmeny:";
+    var_dump($tomb);
+    echo"<br> Eredmeny:".' ';
+    sort($tomb);
+	for($i=0;$i<7;$i++) {
+       
+	echo  $tomb[$i]. ' ';
+    
+}
+?>
