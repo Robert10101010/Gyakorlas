23db67016d176651e5c180dbdad65210fbe1f466
Robert10101010 <109917542+Robert10101010@users.noreply.github.com>
2022-08-18 15:32:57 +0200

    Add files via upload

--- HF-7.php
+++ HF-7.php
@@ -3,23 +3,6 @@ Tipp:
 <input type="text" name="number" value=''/> <br><br>
 Eredmeny: 
 <?php 
-$szam= $_POST['number'];
-$submitbutton= $_POST['button'];
-$randomnumber= mt_rand(1,1000);
-if ($submitbutton){
-
-if ($szam > $randomnumber) {
-    echo "  A szam nagyobb probalkozz ujra";
-    }
-elseif ($szam < $randomnumber) { 
-    echo " A szam kisebb probalkozz ujra";
-    }
-else {
-    echo "$randomnumber. Eltalatad.";
-    }
-}
-?>
-<?php
 session_start();
 if(!isset($_SESSION['counter'])) {
     $_SESSION['counter'] = 0;
@@ -29,6 +12,20 @@ if(isset($_POST['button'])) {
 }    
 if(isset($_POST['reset'])) {
     $_SESSION['counter'] = 0;
+}
+$szam= $_POST['number'];
+$submitbutton= $_POST['button'];
+$randomnumber= mt_rand(1,1000);
+if ($submitbutton){
+if ($szam > $randomnumber) {
+    echo "  A szam nagyobb probalkozz ujra";
+    }
+elseif ($szam < $randomnumber) { 
+    echo " A szam kisebb probalkozz ujra";
+    }
+else {
+    echo "$randomnumber. Eltalatad.";
+    }
 }
 ?>
 <br><?php echo $_SESSION['counter']; ?><br>
