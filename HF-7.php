65d1d9f778b4b7c1c6671ddb17d8c6d65e418cd6
Robert10101010 <109917542+Robert10101010@users.noreply.github.com>
2022-08-18 15:17:03 +0200

    Add files via upload

---
+++ HF-7.php
@@ -0,0 +1,40 @@
+<form action="" method="POST">
+Tipp:
+<input type="text" name="number" value=''/> <br><br>
+Eredmeny: 
+<?php 
+$szam= $_POST['number'];
+$submitbutton= $_POST['button'];
+$randomnumber= mt_rand(1,1000);
+if ($submitbutton){
+
+if ($szam > $randomnumber) {
+    echo "  A szam nagyobb probalkozz ujra";
+    }
+elseif ($szam < $randomnumber) { 
+    echo " A szam kisebb probalkozz ujra";
+    }
+else {
+    echo "$randomnumber. Eltalatad.";
+    }
+}
+?>
+<?php
+session_start();
+if(!isset($_SESSION['counter'])) {
+    $_SESSION['counter'] = 0;
+}
+if(isset($_POST['button'])) {
+    ++$_SESSION['counter'];
+}    
+if(isset($_POST['reset'])) {
+    $_SESSION['counter'] = 0;
+}
+?>
+<br><?php echo $_SESSION['counter']; ?><br>
+<input type="submit" name="button" value="Ellenorzes"/><br>
+<input type="hidden" name="counter" value="<?php echo $_SESSION['counter']; ?>" />
+<input type="submit" name="reset" value="Reset" />
+</form>
+
+
